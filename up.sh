f47b3b3d79f70ca54efe1ea1ab6826016b91d63c
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-08-15 21:22:07 +0000

    update snapshots

--- up.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update && sudo apt-get install -y \
-  libgl1 libegl1 libglib2.0-0 libfontconfig1 libdbus-1-3 libnss3 \
-  libxkbcommon0 libxkbcommon-x11-0 libxcb-cursor0 libxcb-icccm4 \
-  libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 \
-  libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0
-
-export QT_QPA_PLATFORM=offscreen
-pytest tests/test_snapshots.py --snapshot-update
