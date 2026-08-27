578c0a75dc8f670bf3d7378d72da56c21a086962
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-08-27 09:51:51 -0400

    Create in.sh [skip ci]

---
+++ in.sh
@@ -0,0 +1,6 @@
+pip install -r requirements.txt -r tests/requirements-dev.txt
+sudo apt-get update
+sudo apt-get install -y libgl1 libegl1 libxkbcommon-x11-0 libdbus-1-3 \
+  libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
+  libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 \
+  libxcb-xfixes0 libxcb-xkb1
